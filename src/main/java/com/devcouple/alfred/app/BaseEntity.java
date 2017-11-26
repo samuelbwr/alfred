a5dca78b25aea8053c2615779b4486950b9c2253
Samuel Berwanger <samuelbwr@gmail.com>
2017-11-26 19:14:44 -0200

    fixing modeling details

--- src/main/java/com/devcouple/alfred/app/BaseEntity.java
+++ src/main/java/com/devcouple/alfred/app/BaseEntity.java
@@ -4,13 +4,12 @@ package com.devcouple.alfred.app;
 import org.springframework.data.annotation.Id;
 
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
 public abstract class BaseEntity {
 
     @Id
-    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    @GeneratedValue
     private Long id;
 }
